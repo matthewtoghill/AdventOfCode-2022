c848c472b66a422a3e78c291f758e7514cfb47d5
matthewtoghill <matthew.toghill@outlook.com>
2022-12-01 06:27:44 +0000

    Initial commit

---
+++ Day01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day01;
+
+public class Program
+{
+    private static readonly string input = File.ReadAllText(@"..\..\..\..\data\day01.txt");
+    private static void Main()
+    {
+        var elves = input.Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries)
+                         .Select(x => x.Split('\n').Select(int.Parse).Sum())
+                         .OrderDescending()
+                         .ToList();
+
+        Console.WriteLine($"Part 1: {elves[0]}");
+        Console.WriteLine($"Part 2: {elves.Take(3).Sum()}");
+    }
+}
