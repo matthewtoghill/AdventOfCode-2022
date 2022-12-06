6f69551eeb205e8757f8bb2d5a29ac5a52095d97
matthewtoghill <matthew.toghill@outlook.com>
2022-12-06 08:18:23 +0000

    Update day 6

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,15 +1,15 @@
 ﻿var input = Input.ReadAll().AsSpan();
 
-Console.WriteLine($"Part 1: {GetIndexOfFirstUniqueSet(input, 4)}");
-Console.WriteLine($"Part 2: {GetIndexOfFirstUniqueSet(input, 14)}");
+Console.WriteLine($"Part 1: {GetIndexAfterFirstUniqueSet(input, 4)}");
+Console.WriteLine($"Part 2: {GetIndexAfterFirstUniqueSet(input, 14)}");
 
-static int GetIndexOfFirstUniqueSet(ReadOnlySpan<char> span, int setLength)
+static int GetIndexAfterFirstUniqueSet(ReadOnlySpan<char> span, int setLength)
 {
-    for (int i = setLength - 1; i < span.Length; i++)
+    for (int i = setLength; i < span.Length; i++)
     {
-        var set = new HashSet<char>(span.Slice(i - setLength + 1, setLength).ToArray());
+        var set = new HashSet<char>(span.Slice(i - setLength, setLength).ToArray());
         if (set.Count == setLength)
-            return i + 1;
+            return i;
     }
 
     return 0;
