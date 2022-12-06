428b4435f7af900a15142b2092ebf41f6e756b8f
matthewtoghill <matthew.toghill@outlook.com>
2022-12-06 07:06:22 +0000

    Add day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = Input.ReadAll().AsSpan();
+
+Console.WriteLine($"Part 1: {GetIndexOfFirstUniqueSet(input, 4)}");
+Console.WriteLine($"Part 2: {GetIndexOfFirstUniqueSet(input, 14)}");
+
+static int GetIndexOfFirstUniqueSet(ReadOnlySpan<char> span, int setLength)
+{
+    for (int i = setLength - 1; i < span.Length; i++)
+    {
+        var set = new HashSet<char>(span.Slice(i - setLength + 1, setLength).ToArray());
+        if (set.Count == setLength)
+            return i + 1;
+    }
+
+    return 0;
+}
