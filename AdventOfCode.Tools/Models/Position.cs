40a7cbfee7084784eb8e957052888686f9c56e84
matthewtoghill <matthew.toghill@outlook.com>
2022-12-09 21:58:56 +0000

    Add day 9

---
+++ AdventOfCode.Tools/Models/Position.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode.Tools.Models;
+
+public readonly struct Position
+{
+    public readonly int X;
+    public readonly int Y;
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Position MoveInDirection(char dir)
+    {
+        return char.ToLower(dir) switch
+        {
+            'u' => new Position(X, Y - 1),
+            'd' => new Position(X, Y + 1),
+            'l' => new Position(X - 1, Y),
+            'r' => new Position(X + 1, Y),
+            _ => new Position(X, Y),
+        };
+    }
+
+    public int ManhattanDistance(Position other) => (X, Y).ManhattanDistance((other.X, other.Y));
+    public double DirectDistance(Position other) => (X, Y).DirectDistance((other.X, other.Y));
+    public int ChessDistance(Position other) => (X, Y).ChessDistance((other.X, other.Y));
+    public bool Equals(Position other) => X == other.X && Y == other.Y;
+    public override bool Equals(object? obj) => obj is Position p && Equals(p);
+    public override int GetHashCode() => HashCode.Combine(X.GetHashCode(), Y.GetHashCode());
+    public override string ToString() => $"({X}, {Y})";
+
+    public void Deconstruct(out int x, out int y)
+    {
+        x = X;
+        y = Y;
+    }
+
+    public static bool operator ==(Position left, Position right) => left.Equals(right);
+    public static bool operator !=(Position left, Position right) => !left.Equals(right);
+    public static Position operator +(Position left, Position right) => new(left.X + right.X, left.Y + right.Y);
+    public static Position operator -(Position p) => new(-p.X, -p.Y);
+    public static Position operator -(Position left, Position right) => new(left.X - right.X, left.Y - right.Y);
+    public static Position operator *(Position left, Position right) => new(left.X * right.X, right.Y * right.Y);
+    public static Position operator /(Position left, Position right) => new(left.X / right.X, right.Y / right.Y);
+
+}
---
+++ Day09/Program.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Tools.Models;
+
+var instructions = Input.ReadAllLines();
+
+Console.WriteLine($"Part 1: {SimulateRope(2)}");
+Console.WriteLine($"Part 2: {SimulateRope(10)}");
+
+int SimulateRope(int knotCount)
+{
+    var knots = new Position[knotCount];
+    HashSet<Position> visited = new() { knots[^1] };
+
+    foreach (var line in instructions)
+    {
+        var steps = int.Parse(line[1..]);
+
+        for (int i = 0; i < steps; i++)
+        {
+            knots[0] = knots[0].MoveInDirection(line[0]);
+
+            for (int k = 1; k < knots.Length; k++)
+                knots[k] = MoveFollower(knots[k - 1], knots[k]);
+
+            visited.Add(knots[^1]);
+        }
+    }
+
+    return visited.Count;
+}
+
+Position MoveFollower(Position leader, Position follower)
+{
+    var distance = leader.ChessDistance(follower);
+
+    if (distance <= 1)
+        return follower;
+
+    var (distX, distY) = leader - follower;
+    return new(follower.X + Math.Sign(distX), follower.Y + Math.Sign(distY));
+}
